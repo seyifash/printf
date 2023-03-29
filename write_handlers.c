2a0424c16e2ec7fd401556213e37509b8bc7174a
seyifash <fasugbeoluwaseyi@gmail.com>
2023-03-28 22:20:49 -0700

    printf

--- write_handlers.c
+++ write_handlers.c
@@ -29,17 +29,17 @@ int handle_write_char(char c, char buffer[],
 
 	if (width > 1)
 	{
-		buffer[BUFF_SIZE - 1] = '\0';
-		for (i = 0; i < width - 1; i++)
-			buffer[BUFF_SIZE - i - 2] = padd;
+	buffer[BUFF_SIZE - 1] = '\0';
+	for (i = 0; i < width - 1; i++)
+	buffer[BUFF_SIZE - i - 2] = padd;
 
-		if (flags & F_MINUS)
-			return (write(1, &buffer[0], 1) +
-					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
+	if (flags & F_MINUS)
+	return (write(1, &buffer[0], 1) +
+	write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 
-		else
-			return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
-					write(1, &buffer[0], 1));
+	else
+	return (write(1, &buffer[BUFF_SIZE - i - 1], width - 1) +
+	write(1, &buffer[0], 1));
 	}
 	return (write(1, &buffer[0], 1));
 }
@@ -66,16 +66,16 @@ int write_number(int is_negative, int ind, char buffer[],
 	UNUSED(size);
 
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
-		padd = '0';
+	padd = '0';
 	if (is_negative)
-		extra_ch = '-';
+	extra_ch = '-';
 	else if (flags & F_PLUS)
-		extra_ch = '+';
+	extra_ch = '+';
 	else if (flags & F_SPACE)
-		extra_ch = ' ';
+	extra_ch = ' ';
 
 	return (write_num(ind, buffer, flags, width, precision,
-				length, padd, extra_ch));
+	length, padd, extra_ch));
 }
 
 /**
@@ -93,53 +93,50 @@ int write_number(int is_negative, int ind, char buffer[],
  */
 
 int write_num(int ind, char buffer[],
-		int flags, int width, int prec,
-		int length, char padd, char extra_c)
+int flags, int width, int prec,
+int length, char padd, char extra_c)
 {
 	int i, padd_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
-		return (0);
+	return (0);
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-		buffer[ind] = padd = ' ';
+	buffer[ind] = padd = ' ';
 	if (prec > 0 && prec < length)
-		padd = ' ';
+	padd = ' ';
 	while (prec > length)
-		buffer[--ind] = '0', length++;
+	buffer[--ind] = '0', length++;
 	if (extra_c != 0)
-		length++;
+	length++;
 	if (width > length)
 	{
-		for (i = 1; i < width - length + 1;
-				i++)buffer[i] = padd;
+		for (i = 1; i < width - length + 1; i++)
+		buffer[i] = padd;
 		buffer[i] = '\0';
 		if (flags & F_MINUS && padd == ' ')
 		{
-			if (extra_c)
-				buffer[--ind] = extra_c;
-			return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
+		if (extra_c)
+		buffer[--ind] = extra_c;
+		return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 		}
-
 		else if (!(flags & F_MINUS) && padd == ' ')
 		{
-			if (extra_c)
-				buffer[--ind] = extra_c;
-			return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
+		if (extra_c)
+		buffer[--ind] = extra_c;
+		return (write(1, &buffer[1], i - 1) + write(1, &buffer[ind], length));
 		}
-
 		else if (!(flags & F_MINUS) && padd == '0')
 		{
-			if (extra_c)
-				buffer[--padd_start] = extra_c;
-			return (write(1, &buffer[padd_start], i - padd_start) +
-					write(1, &buffer[ind], length - (1 - padd_start)));
+		if (extra_c)
+		buffer[--padd_start] = extra_c;
+		return (write(1, &buffer[padd_start], i - padd_start) +
+		write(1, &buffer[ind], length - (1 - padd_start)));
 		}
 	}
 	if (extra_c)
-		buffer[--ind] = extra_c;
+	buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
 }
-
 /**
  * write_unsgnd - Writes an unsigned number
  * @is_negative: Number indicating if the num is negative
