dd9f5c7adb52f2dbdd93f84cb1326df69810eb69
seyifash <fasugbeoluwaseyi@gmail.com>
2023-03-28 22:39:29 -0700

    printf

--- utils.c
+++ utils.c
@@ -37,6 +37,19 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	return (3);
 }
+/**
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
 
 /**
  * convert_size_number - Casts a number to the specified size
